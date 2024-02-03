dcd53e8db892972606bf7951c740e3431795c6c5
Shawn Seals <shawn.seals915@yahoo.com>
2024-02-03 13:43:13 -0600

    Update ContentView.

--- SongLab/ContentView.swift
+++ SongLab/ContentView.swift
@@ -13,12 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("SongLab!")
         }
         .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
